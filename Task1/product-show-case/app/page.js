a18342d0b0337366b04e218310a188b7402a56e8
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2024-10-28 14:26:06 +0545

    update

--- Task1/product-show-case/app/page.js
+++ Task1/product-show-case/app/page.js
@@ -4,14 +4,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div
-      className="flex flex-col items-center justify-center w-full min-h-screen"
+      className="flex flex-col items-center justify-center w-full min-h-screen px-5"
       style={{ backgroundImage: "url(/bg.svg)" }}
     >
-      <h1 className="pb-10 font-serif text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-slate-50 to-slate-300 ">
+      <h1 className="pb-10 font-serif text-3xl font-bold text-transparent break-words break-all md:text-5xl bg-clip-text bg-gradient-to-r from-slate-50 to-slate-300 ">
         First Project Submission For Frontend
       </h1>
       <Link href={"/dashboard"}>
-        <h2 className="px-5 py-2 text-3xl text-white transition-all border rounded-md cursor-pointer hover:scale-105 hover:text-black hover:bg-white">
+        <h2 className="px-5 py-2 text-2xl text-white transition-all border rounded-md cursor-pointer md:text-3xl hover:scale-105 hover:text-black hover:bg-white">
           Show Project
         </h2>
       </Link>
